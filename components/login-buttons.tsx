f33391d6cd8c8e5a81195269c2f90e1a09c9eced
atlasacopian <atlasacopian@gmail.com>
2025-04-04 21:53:46 -0700

    Update login-buttons.tsx

--- components/login-buttons.tsx
+++ components/login-buttons.tsx
@@ -50,12 +50,6 @@ export default function LoginButtons() {
             Sign in to prevent losing data when clearing browser history
           </div>
           <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
-            <button
-              onClick={() => handleSignIn("google")}
-              className="block w-full text-left px-4 py-2 text-sm text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700"
-              role="menuitem"
-            >
-              SIGN IN WITH GOOGLE
             </button>
             <button
               onClick={() => handleSignIn("apple")}

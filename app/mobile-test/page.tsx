b2f67f0127f0d2a6e2f705fe54f16db28103fef2
atlasacopian <atlasacopian@gmail.com>
2025-04-04 21:08:23 -0700

    Create page.tsx

---
+++ app/mobile-test/page.tsx
@@ -0,0 +1,11 @@
+import { MobileHeader } from "@/components/mobile-header"
+
+export default function MobileTest() {
+  return (
+    <div className="p-4">
+      <h1 className="text-xl mb-4">Mobile Header Test</h1>
+      <MobileHeader />
+    </div>
+  )
+}
+

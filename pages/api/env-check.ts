f4d6e1e9c329555c02c37ce0bc639a778d36d89a
atlasacopian <atlasacopian@gmail.com>
2025-05-09 21:49:56 -0700

    chore: remove conflicting Pages API env-check route

--- pages/api/env-check.ts
+++
@@ -1,5 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).send(process.env.NEXT_PUBLIC_SITE_URL || 'NOT SET');
-} 
